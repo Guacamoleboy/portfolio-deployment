a66a37bdcea2a384ca9b83b3fb28a77b2eff9378
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-03-06 02:09:20 +0100

    fawfafwaf

--- frontend/src/libs/github/GithubColors.ts
+++ frontend/src/libs/github/GithubColors.ts
@@ -1,17 +1,20 @@
 export const getCommitColor = (count: number) => {
     switch (true) {
+
         case count === 0:
             return "rgba(0, 0, 0, 0.1)";
+
         case count <= 2:
-            return "rgb(91, 113, 85)";
+            return "rgb(103, 128, 96)";
         case count <= 4:
-            return "rgb(109, 134, 101)";
+            return "rgb(114, 139, 105)";
         case count <= 7:
             return "rgb(117, 144, 108)";
         case count <= 10:
             return "rgb(120, 147, 111)";
         case count <= 30:
             return "rgb(137, 169, 126)";
+
         case count > 30 && count <= 50:
             return "rgb(255, 191, 0)";   
         case count > 50 && count <= 70:
@@ -20,6 +23,7 @@ export const getCommitColor = (count: number) => {
             return "rgb(235, 125, 0)";  
         case count > 100:
             return "rgb(255, 94, 0)";
+            
         default:
             return "rgba(89, 110, 83)";
     }
